9a74a2f08049401817f72430a5dc31786226ea5a
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2024-08-25 12:26:52 +0200

    Initial commit

---
+++ src/main/java/pjwstk/football_manager/FootballManagerApplication.java
@@ -0,0 +1,13 @@
+package pjwstk.football_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjwstk/football_manager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pjwstk.football_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
