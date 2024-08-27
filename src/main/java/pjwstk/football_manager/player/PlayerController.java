32947345a72def1d2e459fa4da18bdc7b72a7869
JakubPiwowarczyk <jakub.piwowarczyk@gmail.com>
2024-08-27 20:47:42 +0200

    Created PlayerController and an endpoint to login page for player, as well as thymleaf template and css file for this page

---
+++ src/main/java/pjwstk/football_manager/player/PlayerController.java
@@ -0,0 +1,23 @@
+package pjwstk.football_manager.player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "player")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("/login-page")
+    String getLoginPage() {
+        return "login";
+    }
+}
